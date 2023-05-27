088da95df6f0cb91cc47afaba7a01822346341b5
Parag More <paragmore306@gmail.com>
2023-05-27 16:24:11 +0530

    product model and types setup

--- types/types.ts
+++ types/types.ts
@@ -1,42 +1,46 @@
-import { Types } from "mongoose";
+import { Document, Schema, Types } from "mongoose";
 
 interface VariantProperties {
-    [key: string]: string;
-  }
-  
-  interface Variant {
-    properties: VariantProperties;
-    stockQuantity: number;
-  }
-  
-  interface Unit {
-    quantity: number;
-    name: string;
-    conversion?: number;
-  }
-  
-  interface Discount {
-    volumeThreshold: number;
-    discountPercentage: number;
-  }
-  
-  interface Product {
-    storeId: Types.ObjectId;
-    name: string;
-    description?: string;
-    sellsPrice: number;
-    purchasePrice?: number;
-    category: string;
-    variants: Variant[];
-    heroImage?: string;
-    images: string[];
-    slug: string;
-    quantity: number;
-    discounts: Discount[];
-    hsnCode?: string;
-    taxIncluded?: boolean;
-    unit: Unit;
-    purchaseUnit: Unit;
-    gstPercentage?: number;
-    deliveryTime?: string;
-  }
+  [key: string]: string;
+}
+
+interface Variant {
+  properties: VariantProperties;
+  stockQuantity: number;
+}
+
+interface Unit {
+  quantity: number;
+  name: string;
+  conversion?: number;
+}
+
+interface Discount {
+  volumeThreshold: number;
+  discountPercentage: number;
+}
+
+interface Product {
+  storeId: Types.ObjectId;
+  name: string;
+  description?: string;
+  sellsPrice: number;
+  purchasePrice?: number;
+  category: string;
+  variants: Variant[];
+  heroImage?: string;
+  images: string[];
+  slug: string;
+  quantity: number;
+  discounts: Discount[];
+  hsnCode?: string;
+  taxIncluded?: boolean;
+  unit: Unit;
+  purchaseUnit: Unit;
+  gstPercentage?: number;
+  deliveryTime?: string;
+}
+
+interface ProductDocument extends Document, Product {}
+
+export { VariantProperties, Variant, Unit, Discount, Product, ProductDocument };
